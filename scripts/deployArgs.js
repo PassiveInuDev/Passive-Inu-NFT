b7d332348486b92507b02477c2bf6e579fce62ed
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-07-26 18:34:31 -0500

    name

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -5,7 +5,7 @@ module.exports = [
     '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
     '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
     'https://public-pre-ipfs.s3.amazonaws.com/Pass_NFT/json/',
-    'https://public-pre-ipfs.s3.amazonaws.com/Farm_Folk_NFT/assets/FarmFolkReveal.json',
+    'https://ipfs.s3.amazonaws.com/Passive_Inu/json/',
 
     [
         ethers.utils.getAddress('0xC3ea7494c33AbCA4FcEa01331043311756b7e336'),
